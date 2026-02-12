54b3931bf392d778aaaa46a5e0d1d8eaed527782
Shahajahanway <shahajahanway@gmail.com>
2026-02-12 21:44:03 +0530

    done

--- day-15/src/controllers/post.controller.js
+++ day-15/src/controllers/post.controller.js
@@ -1,8 +1,21 @@
 const postModel = require("../models/post.model")
+const ImageKit = require("@imagekit/nodejs")
+const {toFile} = require()
 
+
+const imagekit = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+})
 
 async function createPostController(req,res) {
     console.log(req.body, req.file);
+
+    const file = await imagekit.file.upload({
+        file:new toFile(Buffer.from(req.file.Buffer), 'file'),
+        fileName:"test"
+    })
+
+    res.send(file)
     
 }
 
---
+++ day-16/src/routes/post.routes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const postRouter = express.Router()
+const postController = require("../controllers/post.controller")
+const multer = require("multer")
+const upload = multer({ storage: multer.memoryStorage() })
+
+
+
+/**
+ * POST /api/posts [protected]
+ * - req.body = { caption,image-file }
+ */
+postRouter.post("/", upload.single("chacha"), postController.createPostController)
+
+
+module.exports = postRouter
