df7fa159a2c77c5a8f62dbca506b9f73db67a16a
Shahajahanway <Shahajahanway@gmail.com>
2026-03-07 10:45:37 -0500

    day 30

---
+++ Day-30/server.js
@@ -0,0 +1,9 @@
+import app  from "./src/app";
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is runing on port 3000");
+    
+})
---
+++ Day-30/src/app.js
@@ -0,0 +1,7 @@
+import express from ("express")
+import authRouter from("./routes/auth.routes")
+
+const app = express()
+
+
+export default app
