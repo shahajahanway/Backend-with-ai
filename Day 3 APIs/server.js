84ca29a518a2eee3a7f8c022e743abf41886339f
shahajahanway <shahajahanway@gmail.com>
2026-03-13 11:45:35 +0530

    rest full api just start

--- Day 3 APIs/server.js
+++ Day 3 APIs/server.js
@@ -1,24 +1,17 @@
-const express = require('express')
-
+const express = require("express")
 
 const app = express()
-app.use(express.json())
-const notes = []
 
-app.post("/notes",(req,res)=>{
-    console.log(req.body);
-    
-    res.send('Note is Created hua hain')
-    
-    notes.push(req.body)
+app.get('/',(req,res)=>{
+    res.send("Hello world")
+
+
 })
-
-app.get('/notes',(req,res)=>{
-    res.send(notes)
+app.get('/about',(req,res)=>{
+    res.send("This is About page")
 })
-
 
 app.listen(3000,()=>{
-    console.log("Server runing on port 3000");
+    console.log("Server is running on port 3000");
     
 })
