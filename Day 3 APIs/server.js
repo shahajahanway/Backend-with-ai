4ddd1bc8dcbc9488cf3bc9f4510f020d10c8cbd0
shahajahanway <shahajahanway@gmail.com>
2026-03-15 23:49:38 +0530

    rest api is done

--- Day 3 APIs/server.js
+++ Day 3 APIs/server.js
@@ -1,15 +1,22 @@
 const express = require("express")
 
 const app = express()
-
-app.get('/',(req,res)=>{
-    res.send("Hello world")
+app.use(express.json())
 
 
+const notes = []
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    
+    notes.push(req.body)
+    res.send("Note created")
 })
-app.get('/about',(req,res)=>{
-    res.send("This is About page")
+
+app.get('/notes',(req,res)=>{
+    res.send(notes)
 })
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
