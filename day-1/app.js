6a325a1f967dcce75c69728a08f7aa7f38b7f8ee
shahajahanway <shahajahanway@gmail.com>
2026-07-07 00:30:36 +0530

    server created & server start

---
+++ day-1/app.js
@@ -0,0 +1,16 @@
+
+/* 1> npm i cat-me 
+2> const catMe = require("cat-me") 
+3> console.log(catMe()) to print the cats images. 
+*/
+
+// const catMe = require('cat-me')
+
+// console.log(catMe());
+
+const express = require('express')
+
+
+const app = express() // server created
+
+app.listen(3000) // server ko start karta hain
