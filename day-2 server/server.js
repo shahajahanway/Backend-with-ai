8307c6740ec4f3f32b83981d1955f234f243ef07
shahajahanway <shahajahanway@gmail.com>
2026-07-14 17:46:59 +0530

    server done

--- day-2 server/server.js
+++ day-2 server/server.js
@@ -1,8 +1,18 @@
-const app = require("./src/app");
+const express = require('express')
 
+const app = express()
 
+app.get('/',(req,res)=>{
+    res.send('Hello world')
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-    
 })
+
+app.get('/about',(req,res)=>{
+    res.send('This is About Page')
+})
+
+app.get('/home',(req,res)=>{
+    res.send("This is Home Page")
+})
+
+app.listen(3000)
