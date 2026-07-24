b51fe7b29f7d5ea5dadd623ea831841968135d6c
Shahajahan <your.shahajahanway@gmail.com>
2026-07-24 11:57:52 +0530

    feat: implement CRUD oprations for notes API

--- Day-3 APIs/server.js
+++ Day-3 APIs/server.js
@@ -1,24 +1,16 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 app.use(express.json())
 
+const notes = [];
 
-const notes = []
+app.post("/notes", (req, res) => {
+  console.log(req.body);
 
-app.post('/notes',(req,res)=>{
-    console.log(req.body);
-    
-    notes.push(req.body)
-    res.send("Note created")
-})
+  res.send("note created Bhai");
+});
 
-app.get('/notes',(req,res)=>{
-    res.send(notes)
-})
-
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-    
-})
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
